e742d12f03f491b4cf0b471f21f1d68a2e72170d
quarkusio <no-reply@quarkus.io>
2022-04-25 12:10:18 +0000

    Initial commit

---
+++ src/test/java/pad/xavdat/pylon/babyshower/GreetingResourceIT.java
@@ -0,0 +1,9 @@
+package pad.xavdat.pylon.babyshower;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class GreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
